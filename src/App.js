4145ac9f6f94895644de80389257e19d75807407
Dmitry Sky <dmitry.sky@icloud.com>
2016-10-31 12:24:08 -0600

    add speed to app state object and link app state to firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      speed: 10
+    };
+  }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const speedRef = rootRef.child('speed')
+    speedRef.on('value', snap => {
+      this.setState({
+        speed: snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +30,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.speed}
         </p>
       </div>
     );

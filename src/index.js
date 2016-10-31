ce89ef2d52c44f25e443b686bcb1334c37659cda
Dmitry Sky <dmitry.sky@icloud.com>
2016-10-31 11:51:51 -0600

    add firebase to application

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDw9Hx9ZFplOok0h0bZJXTthOqjG3p316w",
+  authDomain: "fir-react-starter-b4872.firebaseapp.com",
+  databaseURL: "https://fir-react-starter-b4872.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "581406812155"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,

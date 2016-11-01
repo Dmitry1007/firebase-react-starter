6ea0c58de905194f97412de6de16b234669c0d27
Dmitry Sky <dmitry.sky@icloud.com>
2016-10-31 18:09:36 -0600

    model email input form submition, listen for input change, grab value

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('Text field value is: ' + this.state.value);
+  }
+
   render() {
     return (
       <div className="container">
-        <input id="txtEmail" type="email" placeholder="Email">
-        </input>
+        <input id="txtEmail" type="email" placeholder="Email" value={this.state.value}
+          onChange={this.handleChange} />
 
         <input id="txtPassword" type="password" placeholder="Password">
         </input>
